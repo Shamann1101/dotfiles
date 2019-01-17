feac41391728ffa3bf92fe5edb0c82ea785ec354
Alexey Terentyev <alexey@terentyev.pro>
2019-01-17 13:17:49 +0700

    Docker install edit

--- init/docker.sh
+++ init/docker.sh
@@ -3,12 +3,21 @@
 # Установка docker
 
 sudo apt-get update
-sudo apt-get -y install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo bash -c 'echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list'
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common \
+    jq
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
-sudo apt-cache policy docker-engine
-sudo apt-get -y install docker-engine
+sudo apt-get -y install docker-ce
 # Переносим директорию загруженных образов в /opt/docker
 sudo service docker stop
 if [ -d "/var/lib/docker" ]; then
@@ -20,9 +29,14 @@ if [ -d "/var/lib/docker" ]; then
 fi
 sudo ln -s /opt/docker /var/lib/docker
 sudo service docker start
+sudo docker run --rm hello-world
 # Снимаем блокировку паролем
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-sudo bash -c "curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-sudo chmod +x /usr/local/bin/docker-compose
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod +x $DESTINATION
+docker --version
+docker-compose --version
